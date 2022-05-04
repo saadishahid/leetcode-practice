3cf5212f4f57438daaef87262255e75acb5d4418
saadishahid <39744515+saadishahid@users.noreply.github.com>
2022-05-04 12:31:44 -0700

    Implemented Reverse the linked List....yes now I am ready for a coding interview

---
+++ Easy/0206-ReverseLinkedList/ReverseLinkedList.js
@@ -0,0 +1,56 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+
+Constraints:
+
+    The number of nodes in the list is the range [0, 5000].
+    -5000 <= Node.val <= 5000
+
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+
+*/
+
+/*
+Approach:
+We initialize two pointers, one for the current node and one for the previous node.
+Since the prevNode does not exist for the head of the list, the previous pointer is set to null.
+We then set the current pointer to the head of the list.
+We initiate the loop and essentially we need to point the nextPointer to the previous node.
+We then set the current node to the next node.
+However, we would need to store the next node in a temporary variable. This is because we need to set the next node to the previous node.
+By doing so, we will be be losing a pointer to the next node and we need to preseve that
+By the end of the loop, we will have the previous node pointing to the head of the list.s
+*/
+
+var reverseList = function (head) {
+    let prevPointer = null;
+    let currPointer = head;
+
+
+    while (currPointer) {
+        let nextTempPointer = currPointer.next;
+        currPointer.next = prevPointer;
+        prevPointer = currPointer;
+        currPointer = nextTempPointer;
+    }
+    return prevPointer;
+
+};
